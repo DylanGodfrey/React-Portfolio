b3adcdbb094600b0d6f2b2b765c63f97cda94ffd
DylanGodfrey <39961099+DylanGodfrey@users.noreply.github.com>
2022-11-22 16:06:39 -0500

    Added images to projectws

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,6 +6,7 @@ export default function Project({project}) {
       <h3>{project.title}</h3>
       <p>{project.description}</p>
       <a className="btn" href={project.github}>View GitHub</a>
+      <a className="btn" href={project.deployed}>View Deployed Application</a>
       <section className="technologies">
         {project.technologies.map(tech => { return (<div>{tech}</div>); })}
         </section>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,36 +6,48 @@ const projects = [
   title: "Good Food Reviews",
   description: "Socialize with other food critics/lovers.",
   github: "https://github.com/DianasJourney/GoodFoodReviews",
+  deployed: "https://goodie-foodies-heroku.herokuapp.com/",
+  src: "../assets/goodfood.jpg",
   technologies: ["Heroku", "Sequelize", "Express", "Handlebars", "SemanticUI"]
   },
   {
     title: "Where2Watch",
     description: "Easily find out which movies are streaming and on what platform",
     github: "https://github.com/DylanGodfrey/Where2Watch",
+    deployed: "https://dylangodfrey.github.io/Where2Watch/",
+    src: "../assets/where2watch.png",
     technologies: ["Tailwind", "WebAPI"]
   },
   {
-    title: "Note-Taker",
+    title: "Note Taker",
     description: "Keep track of important notes all in one place",
-    github: "https://github.com/DylanGodfrey/Note-Taker",
+    github: "https://github.com/DylanGodfrey/Note-Taker",  
+    deployed: "https://goodie-foodies-heroku.herokuapp.com/",
+    src: "../assets/notetaker.png",
     technologies: ["Express"]
   },
   {
     title: "Just Another Text Editor",
     description: "Let anyone write code with Javascript syntaxing online or off",
     github: "https://github.com/DylanGodfrey/Just-Another-Text-Editor",
+    deployed: "https://nameless-meadow-18411.herokuapp.com/",
+    src: "../assets/jate.png",
     technologies: ["Express", "PWA"]
   },  
   {
-    title: "Employee Tracker",
-    description: "View, edit and create new employees, roles or departments through the command line.",
-    github: "https://github.com/DylanGodfrey/Employee-Tracker",
+    title: "Work Day Scheduler",
+    description: "Keep track of their busy work day schedules through an easy-to-use web interface.",
+    github: "https://github.com/DylanGodfrey/Work-Day-Scheduler",
+    deployed: "https://dylangodfrey.github.io/Work-Day-Scheduler/",
+    src: "../assets/workday.png",
     technologies: ["Inquirer", "MySQL"]
   },
   {
-    title: "Social Network API",
-    description: "Share your thoughts and make friends",
-    github: "https://github.com/DylanGodfrey/Social-Network-API",
+    title: "Team Profile Generator",
+    description: "Quickly create a team portfolio web page through a few short prompts on the command line.",
+    github: "https://github.com/DylanGodfrey/Team-Profile-Generator",
+    deployed: "",
+    src: "",
     technologies: ["Express", "Mongoose"]
   }
 ]
