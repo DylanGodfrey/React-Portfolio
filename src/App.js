c7db30165c34a7af8160c47f31a278b56e4868d8
DylanGodfrey <39961099+DylanGodfrey@users.noreply.github.com>
2022-11-22 15:04:51 -0500

    Created file structure, primitive routing logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
+// Import state management
+import { useState } from 'react';
+
+// Import components and pages
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import AboutMe from './pages/AboutMe';
+import Projects from './pages/Projects';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+
+// Root Component
 function App() {
+  // State variable to determine current page
+  const [page, setPage] = useState('about');
+
+  // Render state based on page state
+  function RenderPage(){
+    switch(page){
+      case "about":
+        return <AboutMe />
+      case "projects":
+        return <Projects />
+      case "resumes":
+        return <Resume />
+      case "contact":
+        return <Contact />
+      default:
+        return <AboutMe />
+    }
+  }
   return (
+    // 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav page={page} setPage={setPage}/>
+      <RenderPage />
+      <Footer />
     </div>
   );
 }
